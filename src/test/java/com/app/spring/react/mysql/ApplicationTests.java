e17a1b1cddb94bdb20ebff8c6062f9fdd523846d
Marc Fowler <marc.fowler.email@gmail.com>
2023-04-21 19:30:56 -0400

    initial commit

---
+++ src/test/java/com/app/spring/react/mysql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.spring.react.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
