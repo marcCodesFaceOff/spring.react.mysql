3ee46aef489e14cbfd06a511bcf898ca5d4d6995
Marc Fowler <marc.fowler.email@gmail.com>
2023-04-22 15:45:49 -0400

    add controller class

--- src/main/java/com/app/spring/react/mysql/controller/RosterController.java
+++ src/main/java/com/app/spring/react/mysql/controller/RosterController.java
@@ -1,6 +1,52 @@
 package com.app.spring.react.mysql.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.app.spring.react.mysql.modal.Person;
+import com.app.spring.react.mysql.service.RosterService;
+@RestController
+@RequestMapping("/api")
 public class RosterController {
 
+    @Autowired
+    private RosterService rosterService;
 
+    @GetMapping("/roster")
+    public List<Person> get() {
+        return rosterService.get();
+    }
+
+    @PostMapping("/roster")
+    public Person save(@RequestBody Person person) {
+        rosterService.save(person);
+        return person;
+    }
+
+    @GetMapping("/roster/{bed}")
+    public Person get(@PathVariable int bed) {
+        return rosterService.get(bed);
+    }
+
+    @DeleteMapping("/roster/{id}")
+    public String delete(@PathVariable int bed) {
+
+        rosterService.delete(bed);
+
+        return "Roster removed person at bed "+bed;
+
+    }
+
+    @PutMapping("/roster")
+    public Person update(@RequestBody Person person) {
+        rosterService.save(person);
+        return person;
+    }
 }
