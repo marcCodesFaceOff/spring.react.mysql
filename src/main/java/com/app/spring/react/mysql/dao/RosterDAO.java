953d5adb6a03b855c364095792aa128362ac5bf4
Marc Fowler <marc.fowler.email@gmail.com>
2023-04-22 13:40:14 -0400

    adds rosterDAO

--- src/main/java/com/app/spring/react/mysql/dao/RosterDAO.java
+++ src/main/java/com/app/spring/react/mysql/dao/RosterDAO.java
@@ -1,4 +1,14 @@
 package com.app.spring.react.mysql.dao;
 
-public class RosterDAO {
+import java.util.List;
+import com.app.spring.react.mysql.modal.Person;
+public interface RosterDAO {
+
+    List<Person> get();
+
+    Person get(int bed);
+
+    void save(Person employee);
+
+    void delete(int bed);
 }
