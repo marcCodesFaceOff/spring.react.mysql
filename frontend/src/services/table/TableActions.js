c687a85447b94f9c99ecd6e1401627a82f7cef1d
Marc Fowler <marc.fowler.email@gmail.com>
2023-05-29 13:45:00 -0400

    updates table

--- frontend/src/services/table/TableActions.js
+++ frontend/src/services/table/TableActions.js
@@ -1,32 +1,61 @@
 import * as ROSTER from "./rosterTypes";
 import axios from "axios";
 
-export const deletePerson = (bedID) => {
-    return (dispatch) => {
-      dispatch({
-        type: ROSTER.DELETE_PERSON_REQUEST,
-      });
-      axios
-        .delete("http://localhost:8080/api/roster/" + bedID)
-        .then((response) => {
-          dispatch(rosterSuccess(response.data));
-        })
-        .catch((error) => {
-          dispatch(rosterFailure(error));
-        });
-    };
-  };
+const url = "http://localhost:8080/api/roster";
 
-  const rosterSuccess = (book) => {
-    return {
-      type: ROSTER.ROSTER_SUCCESS,
-      payload: book,
-    };
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+};
+
+export const deletePerson = (bedID) => {
+  console.log("Inside DeletePerson!!!");
+  return (dispatch) => {
+    dispatch({
+      type: ROSTER.DELETE_PERSON_REQUEST,
+    });
+    axios
+      .delete(url + bedID)
+      .then((response) => {
+        dispatch(rosterSuccess(response.data));
+      })
+      .catch((error) => {
+        dispatch(rosterFailure(error));
+      });
   };
-  
-  const rosterFailure = (error) => {
-    return {
-      type: ROSTER.ROSTER_FAILURE,
-      payload: error,
-    };
+};
+
+export const getRoster = () => {
+  return (dispatch) => {
+    dispatch({
+      type: ROSTER.FETCH_ROSTER_REQUEST,
+    });
+    axios
+      .get(url, {}, config)
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({
+          roster: data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+};
+
+const rosterSuccess = (roster) => {
+  return {
+    type: ROSTER.ROSTER_SUCCESS,
+    payload: roster,
+  };
+};
+
+const rosterFailure = (error) => {
+  return {
+    type: ROSTER.ROSTER_FAILURE,
+    payload: error,
+  };
+};
